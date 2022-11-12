bf3c6fac4807843c0feb90b81b9731ea30e3dac2
(bsh714) <(natebsh@nate.com)>
2022-11-12 16:40:02 +0900

    파이썬 내용 삭제

--- app.py
+++ app.py
@@ -24,12 +24,3 @@ def mars_post():
 
     db.orders.insert_one(doc)
 
-    return jsonify({'msg': '주문 완료!'})
-
-@app.route("/mars", methods=["GET"])
-def mars_get():
-    orders_list = list(db.orders.find({},{'_id':False}))
-    return jsonify({'orders':orders_list})
-
-if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
